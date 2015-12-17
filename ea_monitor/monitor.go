fd380570aa26fadf0ddee9b54b7041fe5f016533
Rob Tuley <rob@openknot.com>
2015-12-17 20:20:29 +0000

    Added pagination for EA gauge discovery

--- ea_monitor/monitor.go
+++ ea_monitor/monitor.go
@@ -3,6 +3,7 @@ package main
 import (
 	"encoding/json"
 	"net/http"
+	"strconv"
 	"time"
 
 	"github.com/robtuley/report"
@@ -23,9 +24,11 @@ func main() {
 
 	apiRequestThrottle = time.Tick(time.Second / API_REQUESTS_PER_SECOND)
 	wiskiIdC := discoverEaGaugeWiskiIDs()
-	for id := range wiskiIdC {
-		report.Info("discover.found", report.Data{"wiskiId": id})
+	n := 0
+	for range wiskiIdC {
+		n = n + 1
 	}
+	report.Info("discover.found", report.Data{"count": n})
 }
 
 func discoverEaGaugeWiskiIDs() chan string {
@@ -33,43 +36,53 @@ func discoverEaGaugeWiskiIDs() chan string {
 
 	go func() {
 		type StationList struct {
-			Meta struct {
-				Limit  int `json:"limit"`
-				Offset int `json:"offset"`
-			} `json:"meta"`
 			Items []struct {
 				WiskiID string `json:"wiskiId"`
 			} `json:"items"`
 		}
 
-		url := "http://environment.data.gov.uk/flood-monitoring/id/stations?_limit=50"
+		batchSize := 100
+		lastBatchSize := batchSize
+		currentOffset := 0
+		baseUrl := "http://environment.data.gov.uk/flood-monitoring/id/stations" +
+			"?_limit=" + strconv.Itoa(batchSize)
 		client := &http.Client{}
 
-		<-apiRequestThrottle
-		req, err := http.NewRequest("GET", url, nil)
-		if err != nil {
-			report.Action("discover.request.error", report.Data{"error": err.Error()})
-			return
-		}
-		req.Header.Add("Accept", "application/json")
-		req.Header.Set("User-Agent", API_USER_AGENT)
-		resp, err := client.Do(req)
-		if err != nil {
-			report.Action("discover.request.error", report.Data{"error": err.Error()})
-			return
-		}
-		report.Info("discover.dump", report.Data{"response": resp})
-		decoder := json.NewDecoder(resp.Body)
-		s := StationList{}
-		err = decoder.Decode(&s)
-		resp.Body.Close()
-		if err != nil {
-			report.Action("discover.decode.error", report.Data{"error": err.Error()})
-			return
-		}
+		// The paging _limit and _offset parameters apply to the number of 'measures'
+		// in the EA API result set rather than the number of items, so simply iterate
+		// until we receive a completely empty set.
+		for lastBatchSize > 0 {
+			<-apiRequestThrottle
 
-		for _, item := range s.Items {
-			idC <- item.WiskiID
+			url := baseUrl + "&_offset=" + strconv.Itoa(currentOffset)
+			req, err := http.NewRequest("GET", url, nil)
+			if err != nil {
+				report.Action("discover.request", report.Data{"error": err.Error()})
+				return
+			}
+			req.Header.Add("Accept", "application/json")
+			req.Header.Set("User-Agent", API_USER_AGENT)
+			resp, err := client.Do(req)
+			if err != nil {
+				report.Action("discover.request", report.Data{"error": err.Error()})
+				return
+			}
+			decoder := json.NewDecoder(resp.Body)
+			s := StationList{}
+			err = decoder.Decode(&s)
+			resp.Body.Close()
+			if err != nil {
+				report.Action("discover.decode", report.Data{"error": err.Error()})
+				return
+			}
+
+			for _, item := range s.Items {
+				idC <- item.WiskiID
+			}
+
+			lastBatchSize = len(s.Items)
+			report.Info("discover.request", report.Data{"count": lastBatchSize})
+			currentOffset = currentOffset + batchSize
 		}
 
 		close(idC)
