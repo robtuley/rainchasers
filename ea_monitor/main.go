9999b648e506938a738417934d6189fe6694a86b
Rob Tuley <rob@openknot.com>
2015-12-20 20:21:40 +0000

    Drafted skeleton code for reading updates every 15 mins

--- ea_monitor/main.go
+++ ea_monitor/main.go
@@ -13,13 +13,20 @@ func main() {
 	report.RuntimeStatsEvery(30 * time.Second)
 
 	refSnapC := make(chan *Snapshot, 10)
-
 	for url := range discoverStationUrls() {
 		_, measures := requestStationDetail(url)
 		for _, m := range measures {
 			refSnapC <- &m
 		}
 	}
+
+	updateSnapC := make(chan *SnapshotUpdate, 10)
+	ticker := time.NewTicker(time.Minute * 15)
+	go func() {
+		for range ticker.C {
+			requestLatestReadings(updateSnapC)
+		}
+	}()
 
 	//pubSnapC := applyUpdatesToReferenceSnapshots(refSnapC, updateSnapC)
 }
---
+++ ea_monitor/update.go
@@ -0,0 +1,64 @@
+package main
+
+import (
+	"encoding/json"
+	"strconv"
+	"time"
+
+	"github.com/robtuley/report"
+)
+
+type readingJson struct {
+	Items []struct {
+		Measure  string    `json:"measure"`
+		DateTime time.Time `json:"dateTime"`
+		Value    float32   `json:"value"`
+	} `json:"items"`
+}
+
+func requestLatestReadings(updateC chan *SnapshotUpdate) {
+	batchSize := 100
+	lastBatchSize := batchSize
+	currentOffset := 0
+	baseUrl := "http://environment.data.gov.uk/flood-monitoring/data/readings" +
+		"latest&_limit=" + strconv.Itoa(batchSize)
+
+	for lastBatchSize < batchSize {
+		waitOnApiRequestSchedule()
+
+		url := baseUrl + "&_offset=" + strconv.Itoa(currentOffset)
+		currentOffset = currentOffset + batchSize
+		tick := report.Tick()
+
+		err, resp := doJsonRequest(url)
+		if err != nil {
+			report.Action("update.request.error", report.Data{"url": url, "error": err.Error()})
+			resp.Body.Close()
+			continue
+		}
+
+		r := readingJson{}
+		decoder := json.NewDecoder(resp.Body)
+		err = decoder.Decode(&r)
+		resp.Body.Close()
+		if err != nil {
+			report.Action("update.decode.error", report.Data{"url": url, "error": err.Error()})
+			continue
+		}
+
+		lastBatchSize = len(r.Items)
+		report.Tock(tick, "update.response", report.Data{
+			"count": lastBatchSize,
+			"url":   url,
+		})
+
+		for _, item := range r.Items {
+			updateC <- &SnapshotUpdate{
+				item.Measure,
+				item.DateTime,
+				item.Value,
+			}
+		}
+	}
+
+}
