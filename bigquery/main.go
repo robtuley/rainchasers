96139fd5b4d8e9364affc8e65754b13395e5649f
Rob Tuley <rob@openknot.com>
2016-01-19 22:19:20 +0000

    Basic skeleton snapshot stream consumption

---
+++ bigquery/main.go
@@ -0,0 +1,54 @@
+package main
+
+import (
+	"os"
+	"time"
+
+	"github.com/rainchasers/com.rainchasers.gauge/gauge"
+	"github.com/robtuley/report"
+)
+
+// Responds to environment variables:
+//   GCLOUD_PROJECT_ID (no default)
+//   GCLOUD_PUBSUB_TOPIC (no default)
+//
+func main() {
+
+	// setup telemetry and logging
+	defer report.Drain()
+	report.StdOut()
+	report.Global(report.Data{"service": "bigq", "daemon": time.Now().Unix()})
+	report.RuntimeStatsEvery(30 * time.Second)
+
+	// parse env vars
+	projectId := os.Getenv("GCLOUD_PROJECT_ID")
+	topicName := os.Getenv("GCLOUD_PUBSUB_TOPIC")
+	report.Info("daemon.start", report.Data{
+		"project_id":   projectId,
+		"pubsub_topic": topicName,
+	})
+
+	// consume snapshots from pubsub
+	ctx, err := gauge.NewContext(projectId, topicName)
+	if err != nil {
+		report.Action("gpubsub.connect.error", report.Data{"error": err.Error()})
+		return
+	}
+	snapC, errC, err := gauge.Consume(ctx, "snap-to-bigquery")
+	if err != nil {
+		report.Action("gpubsub.consume.error", report.Data{"error": err.Error()})
+		return
+	}
+	go func() {
+		for err := range errC {
+			report.Action("gpubsub.consume.error", report.Data{"error": err.Error()})
+		}
+	}()
+
+	// consume & log
+	for s := range snapC {
+		report.Action("gpubsub.consume.ok", report.Data{"snapshot": s})
+	}
+
+	report.Info("daemon.stop", report.Data{})
+}
