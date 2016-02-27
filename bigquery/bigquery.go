041cc2b0cbe38837cc1084fce794fb595dbac634
Rob Tuley <rob@openknot.com>
2016-02-27 20:15:20 +0000

    Add allow large resultset option to bigquery accumulator

--- bigquery/bigquery.go
+++ bigquery/bigquery.go
@@ -99,6 +99,7 @@ func dedupThisTable(client *bigquery.Client, table *bigquery.Table) (BigQueryJob
 	job, err := client.Copy(
 		context.Background(), table,
 		query, bigquery.WriteTruncate,
+		bigquery.AllowLargeResults(),
 	)
 
 	status := BigQueryJobStatus{
