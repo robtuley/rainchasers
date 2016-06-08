c9721e4ce7d71e01d844d74ae76cb2124adc9c6f
Rob Tuley <rob@openknot.com>
2016-06-08 21:15:54 +0100

    Removed pagination through latest readings updates

--- ea_monitor/update.go
+++ ea_monitor/update.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"encoding/json"
-	"strconv"
 	"time"
 
 	"github.com/rainchasers/com.rainchasers.gauge/gauge"
@@ -14,62 +13,48 @@ type readingJson struct {
 		Measure      string          `json:"measure"`
 		DateTime     time.Time       `json:"dateTime"`
 		ValueRawJson json.RawMessage `json:"value"`
-		ValueParsed  float32
 	} `json:"items"`
 }
 
 func requestLatestReadings(updateC chan gauge.SnapshotUpdate) {
-	const BATCHSIZE = 100
+	url := "http://environment.data.gov.uk/flood-monitoring/data/readings?latest"
+	waitOnApiRequestSchedule()
+	tick := report.Tick()
 
-	lastBatchSize := BATCHSIZE
-	currentOffset := 0
-	baseUrl := "http://environment.data.gov.uk/flood-monitoring/data/readings" +
-		"?latest&_limit=" + strconv.Itoa(BATCHSIZE)
-
-	for lastBatchSize > 0 {
-		waitOnApiRequestSchedule()
-
-		url := baseUrl + "&_offset=" + strconv.Itoa(currentOffset)
-		currentOffset = currentOffset + BATCHSIZE
-		tick := report.Tick()
-
-		resp, err := doJsonRequest(url)
-		if err != nil {
-			report.Action("update.request.error", report.Data{"url": url, "error": err.Error()})
-			continue
-		}
-
-		r := readingJson{}
-		decoder := json.NewDecoder(resp.Body)
-		err = decoder.Decode(&r)
-		resp.Body.Close()
-		if err != nil {
-			report.Action("update.decode.error", report.Data{"url": url, "error": err.Error()})
-			continue
-		}
-
-		lastBatchSize = len(r.Items)
-		report.Tock(tick, "update.response", report.Data{
-			"count": lastBatchSize,
-			"url":   url,
-		})
-
-		for _, item := range r.Items {
-			// the 'value' keys should be a float, but instances exist of arrays
-			// so we do a conditional parse and simply dump those that can't match.
-			err := json.Unmarshal(item.ValueRawJson, &item.ValueParsed)
-			if err != nil {
-				report.Info("update.corrupt.value",
-					report.Data{"url": item.Measure, "json": item.ValueRawJson})
-				continue
-			}
-
-			updateC <- gauge.SnapshotUpdate{
-				Url:      item.Measure,
-				DateTime: item.DateTime,
-				Value:    item.ValueParsed,
-			}
-		}
+	resp, err := doJsonRequest(url)
+	if err != nil {
+		report.Action("update.request.error", report.Data{"url": url, "error": err.Error()})
+		return
 	}
 
+	r := readingJson{}
+	decoder := json.NewDecoder(resp.Body)
+	err = decoder.Decode(&r)
+	resp.Body.Close()
+	if err != nil {
+		report.Action("update.decode.error", report.Data{"url": url, "error": err.Error()})
+		return
+	}
+
+	report.Tock(tick, "update.response", report.Data{
+		"count": len(r.Items),
+		"url":   url,
+	})
+
+	for _, item := range r.Items {
+		// the 'value' keys should be a float, but instances exist of arrays
+		// so we do a conditional parse and simply dump those that can't match.
+		value, err := parseFloatFromScalarOrArray(item.ValueRawJson)
+		if err != nil {
+			report.Info("update.value.missing",
+				report.Data{"url": item.Measure, "json": item.ValueRawJson})
+			continue
+		}
+
+		updateC <- gauge.SnapshotUpdate{
+			Url:      item.Measure,
+			DateTime: item.DateTime,
+			Value:    value,
+		}
+	}
 }
