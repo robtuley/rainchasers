a7db93de3a494149581ea49d4a9a0f0608a0ab47
Rob Tuley <rob@openknot.com>
2017-03-14 08:03:57 +0000

    Brutally moved old code into history area, history is broken

---
+++ ea/history/main.go
@@ -0,0 +1,111 @@
+package main
+
+import (
+	"github.com/robtuley/report"
+	"net/http"
+	"os"
+	"strconv"
+	"time"
+)
+
+const (
+	maxPublishPerSecond  = 20
+	httpTimeoutInSeconds = 300
+	httpUserAgent        = "Rainchaser Bot <hello@rainchasers.com>"
+)
+
+// Responds to environment variables:
+//   DATE (defaults to yesterday)
+//   PROJECT_ID (no default, blank for validation mode)
+//   PUBSUB_TOPIC (no default, blank for validation mode)
+//
+func main() {
+	if err := run(); err != nil {
+		os.Stderr.WriteString(err.Error() + "\n")
+		os.Exit(1)
+	}
+}
+
+func run() error {
+	// setup telemetry and golang defaults
+	report.StdOut()
+	report.Global(report.Data{"service": "ea.history", "daemon": time.Now().Format("v2006-01-02-15-04-05")})
+	report.RuntimeStatsEvery(1 * time.Second)
+
+	// parse env vars
+	updatePeriodSeconds, err := strconv.Atoi(os.Getenv("UPDATE_EVERY_X_SECONDS"))
+	if err != nil {
+		updatePeriodSeconds = 15 * 60
+	}
+	shutdownDeadline, err := strconv.Atoi(os.Getenv("SHUTDOWN_AFTER_X_SECONDS"))
+	if err != nil {
+		shutdownDeadline = 7 * 24 * 60 * 60
+	}
+	projectId := os.Getenv("PROJECT_ID")
+	topicName := os.Getenv("PUBSUB_TOPIC")
+
+	// decision on whether validating
+	isValidating := projectId == ""
+	var logs *LogBuffer
+	if isValidating {
+		logs = trackLogs()
+	}
+	report.Info("daemon.start", report.Data{
+		"update_period":     updatePeriodSeconds,
+		"shutdown_deadline": shutdownDeadline,
+		"project_id":        projectId,
+		"pubsub_topic":      topicName,
+	})
+	shutdownC := time.NewTimer(time.Second * time.Duration(shutdownDeadline)).C
+
+	// discover EA gauging stations
+	refSnapshots, err := discover()
+	if err != nil {
+		report.Action("discovered.fail", report.Data{"error": err.Error()})
+		return err
+	}
+	report.Info("discovered.ok", report.Data{"count": len(refSnapshots)})
+
+	// periodically get latest updates
+	ticker := time.NewTicker(time.Second * time.Duration(updatePeriodSeconds))
+
+updateLoop:
+	for {
+		tick := report.Tick()
+		updates, err := update()
+		if err != nil {
+			report.Action("updated.fail", report.Data{"error": err.Error()})
+			return err
+		}
+		report.Tock(tick, "updated.ok", report.Data{"count": len(updates)})
+
+		if !isValidating {
+			tick = report.Tick()
+			err = publish(projectId, topicName, updates, refSnapshots)
+			if err != nil {
+				report.Action("published.fail", report.Data{"error": err.Error()})
+				return err
+			}
+			report.Tock(tick, "published.ok", report.Data{"count": len(updates)})
+		}
+
+		select {
+		case <-ticker.C:
+		case <-shutdownC:
+			break updateLoop
+		}
+	}
+	ticker.Stop()
+
+	// validate log stream on shutdown if required
+	report.Drain()
+	err = nil
+	if isValidating {
+		expect := map[string]int{
+			"discovered.ok": VALIDATE_IS_PRESENT,
+			"updated.ok":    VALIDATE_IS_PRESENT,
+		}
+		err = validateLogStream(logs, expect)
+	}
+	return err
+}
---
+++ ea/history/validate.go
@@ -0,0 +1,80 @@
+package main
+
+import (
+	"errors"
+	"github.com/robtuley/report"
+	"sync"
+)
+
+const VALIDATE_IS_PRESENT = iota * -1
+
+type LogBuffer struct {
+	Mutex *sync.Mutex
+	Map   map[string][]report.Data
+}
+
+func (b LogBuffer) Add(d report.Data) {
+	evtName := d["event"].(string)
+	b.Mutex.Lock()
+	b.Map[evtName] = append(b.Map[evtName], d)
+	b.Mutex.Unlock()
+}
+
+func (b LogBuffer) Extract() map[string][]report.Data {
+	b.Mutex.Lock()
+	logs := b.Map
+	b.Mutex.Unlock()
+	return logs
+}
+
+func (b LogBuffer) FirstActionableError() error {
+	var err error
+	b.Mutex.Lock()
+	for name, events := range b.Map {
+		for _, e := range events {
+			if e["type"] == "action" {
+				err = errors.New("Actionable error: " + name)
+			}
+		}
+	}
+	b.Mutex.Unlock()
+	return err
+}
+
+func (b LogBuffer) Count() map[string]int {
+	count := make(map[string]int)
+	b.Mutex.Lock()
+	for name, events := range b.Map {
+		count[name] = len(events)
+	}
+	b.Mutex.Unlock()
+	return count
+}
+
+func trackLogs() *LogBuffer {
+	buffer := LogBuffer{
+		Mutex: &sync.Mutex{},
+		Map:   make(map[string][]report.Data),
+	}
+	report.Observe(buffer.Add)
+	return &buffer
+}
+
+func validateLogStream(buffer *LogBuffer, expectCounts map[string]int) error {
+	err := buffer.FirstActionableError()
+	if err != nil {
+		return err
+	}
+
+	count := buffer.Count()
+	for k, expect := range expectCounts {
+		val, exists := count[k]
+		if !exists {
+			return errors.New(k + " expected but not present")
+		}
+		if expect != VALIDATE_IS_PRESENT && expect != val {
+			return errors.New(k + " unexpected count")
+		}
+	}
+	return nil
+}
