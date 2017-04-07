dcc2cf29bdc44480068d22948115dfe654f93467
Rob Tuley <rob@openknot.com>
2017-04-07 22:51:22 +0100

    Fixed snapshot cache purge

--- gauge/cache.go
+++ gauge/cache.go
@@ -8,8 +8,9 @@ import (
 )
 
 type Cache struct {
-	Map   map[string]*CachedSnapshot
-	Mutex *sync.RWMutex
+	snapMap   map[string]*CachedSnapshot
+	rwMutex   *sync.RWMutex
+	retention time.Duration
 }
 
 type CachedSnapshot struct {
@@ -18,38 +19,37 @@ type CachedSnapshot struct {
 	ModifiedAt time.Time
 }
 
-type ReadingSorter []Reading
+type readingSorter []Reading
 
-func (rs ReadingSorter) Len() int {
+func (rs readingSorter) Len() int {
 	return len(rs)
 }
 
-func (rs ReadingSorter) Swap(i, j int) {
+func (rs readingSorter) Swap(i, j int) {
 	rs[i], rs[j] = rs[j], rs[i]
 }
 
-func (rs ReadingSorter) Less(i, j int) bool {
+func (rs readingSorter) Less(i, j int) bool {
 	return rs[i].DateTime.After(rs[j].DateTime)
 }
 
-func NewCache(ctx context.Context, duration time.Duration) *Cache {
-	s := Cache{
-		Mutex: &sync.RWMutex{},
+func NewCache(ctx context.Context, retention time.Duration) *Cache {
+	cache := Cache{
+		rwMutex:   &sync.RWMutex{},
+		retention: retention,
 	}
 
 	// spawn routine to regularly purge cache
 	go func() {
 		purgeInterval := time.Hour
-		if duration < purgeInterval {
-			purgeInterval = duration
+		if cache.retention < purgeInterval {
+			purgeInterval = cache.retention
 		}
 
-		ticker := time.NewTicker(duration)
+		ticker := time.NewTicker(purgeInterval)
 	purgeThenWait:
 		for {
-			s.Mutex.Lock()
-			// DO PURGING
-			s.Mutex.Unlock()
+			cache.purge()
 
 			select {
 			case <-ticker.C:
@@ -60,15 +60,15 @@ func NewCache(ctx context.Context, duration time.Duration) *Cache {
 		ticker.Stop()
 	}()
 
-	return &s
+	return &cache
 }
 
 func (c *Cache) Add(s Snapshot) {
-	c.Mutex.Lock()
-	defer c.Mutex.Unlock()
+	c.rwMutex.Lock()
+	defer c.rwMutex.Unlock()
 
-	if cached, exists := c.Map[s.Station.DataURL]; !exists {
-		c.Map[s.Station.DataURL] = &CachedSnapshot{
+	if cached, exists := c.snapMap[s.Station.UUID()]; !exists {
+		c.snapMap[s.Station.DataURL] = &CachedSnapshot{
 			Station:    s.Station,
 			Readings:   s.Readings,
 			ModifiedAt: time.Now(),
@@ -79,10 +79,27 @@ func (c *Cache) Add(s Snapshot) {
 	}
 }
 
+func (c *Cache) Get(uuid string) (CachedSnapshot, bool) {
+	c.rwMutex.RLock()
+	defer c.rwMutex.RUnlock()
+
+	cached, exists := c.snapMap[uuid]
+	return *cached, exists
+}
+
+func (c *Cache) purge() {
+	c.rwMutex.Lock()
+	defer c.rwMutex.Unlock()
+
+	for k := range c.snapMap {
+		removeOlderThan(time.Now().Add(-1*c.retention), &c.snapMap[k].Readings)
+	}
+}
+
 func concat(a []Reading, b []Reading) []Reading {
 	concat := append(a, b...)
 	removeDuplicates(&concat)
-	sort.Sort(ReadingSorter(concat))
+	sort.Sort(readingSorter(concat))
 	return concat
 }
 
@@ -102,7 +119,7 @@ func removeDuplicates(xs *[]Reading) {
 func removeOlderThan(keepSince time.Time, xs *[]Reading) {
 	j := 0
 	for i, x := range *xs {
-		if keepSince.After(x.DateTime) {
+		if keepSince.Before(x.DateTime) {
 			(*xs)[j] = (*xs)[i]
 			j++
 		}
--- gauge/cache_test.go
+++ gauge/cache_test.go
@@ -43,3 +43,49 @@ func TestConcatDeDuplicatesAndOrdersByTime(t *testing.T) {
 		t.Error("concat() unexpected result", result)
 	}
 }
+
+func TestRemoveOlderThan(t *testing.T) {
+	timestamp, _ := time.Parse(time.RFC3339, "2016-01-01T10:30:00Z")
+	r1 := Reading{
+		DateTime: timestamp,
+		Value:    1.0,
+	}
+	r2 := Reading{
+		DateTime: timestamp.Add(time.Second),
+		Value:    2.0,
+	}
+	r3 := Reading{
+		DateTime: timestamp.Add(10 * time.Second),
+		Value:    3.0,
+	}
+	r4 := Reading{
+		DateTime: timestamp.Add(-1 * time.Second),
+		Value:    4.0,
+	}
+
+	var result []Reading
+
+	result = []Reading{r1, r2, r3, r4}
+	removeOlderThan(timestamp, &result)
+	if !reflect.DeepEqual(result, []Reading{r2, r3}) {
+		t.Error("removeOlderThan unexpected result #1", result)
+	}
+
+	result = []Reading{r3, r1, r2}
+	removeOlderThan(timestamp, &result)
+	if !reflect.DeepEqual(result, []Reading{r3, r2}) {
+		t.Error("removeOlderThan unexpected result #2", result)
+	}
+
+	result = []Reading{r2, r3}
+	removeOlderThan(timestamp, &result)
+	if !reflect.DeepEqual(result, []Reading{r2, r3}) {
+		t.Error("removeOlderThan unexpected result #3", result)
+	}
+
+	result = []Reading{r4}
+	removeOlderThan(timestamp, &result)
+	if !reflect.DeepEqual(result, []Reading{}) {
+		t.Error("removeOlderThan unexpected result #4", result)
+	}
+}
