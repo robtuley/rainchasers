ede1a37d7a2ef93d1467127273db5faeb697d366
Rob Tuley <rob@openknot.com>
2017-04-07 14:45:55 +0100

    Added UUID generation to station type

--- gauge/types.go
+++ gauge/types.go
@@ -1,10 +1,8 @@
 package gauge
 
 import (
-	"crypto/sha256"
-	"encoding/hex"
-	"io"
-	"strings"
+	"crypto/sha1"
+	"fmt"
 	"time"
 )
 
@@ -19,6 +17,14 @@ type Station struct {
 	Unit      string
 }
 
+// valid version 4 UUID (see RFC 4122, section 4.4)
+func (s *Station) UUID() string {
+	b := sha1.Sum([]byte(s.DataURL))
+	b[6] = (b[6] & 0x0f) | 0x40
+	b[8] = (b[8] & 0x3f) | 0x80
+	return fmt.Sprintf("%x-%x-%x-%x-%x", b[0:4], b[4:6], b[6:8], b[8:10], b[10:])
+}
+
 type Reading struct {
 	DateTime time.Time
 	Value    float32
@@ -27,16 +33,4 @@ type Reading struct {
 type Snapshot struct {
 	Station  Station
 	Readings []Reading
-}
-
-func InsertID(s Station, r Reading) string {
-	h := sha256.New()
-	io.WriteString(h, r.DateTime.Format(time.RFC822)+strings.ToLower(s.DataURL))
-	return hex.EncodeToString(h.Sum(nil))
-}
-
-func MetricID(dataURL string) string {
-	h := sha256.New()
-	io.WriteString(h, strings.ToLower(dataURL))
-	return hex.EncodeToString(h.Sum(nil))
 }
--- gauge/types_test.go
+++ gauge/types_test.go
@@ -1,42 +1,19 @@
 package gauge
 
 import (
-	"regexp"
 	"testing"
-	"time"
 )
 
 func TestIdGeneration(t *testing.T) {
-	validHex := regexp.MustCompile(`^[a-f0-9]+$`)
 
-	s := Station{
-		DataURL:   "http://environment.data.gov.uk/flood-monitoring/id/measures/1029TH-level-downstage-i-15_min-mASD",
-		HumanURL:  "http://environment.data.gov.uk/flood-monitoring/id/stations/1029TH",
-		Name:      "Bourton Dickler",
-		RiverName: "Dikler",
-		Lat:       51.874767,
-		Lg:        -1.740083,
-		Type:      "level",
-		Unit:      "metre",
+	s1 := Station{
+		DataURL: "http://environment.data.gov.uk/flood-monitoring/id/measures/1029TH-level-downstage-i-15_min-mASD",
+	}
+	s2 := Station{
+		DataURL: "http://environment.data.gov.uk/flood-monitoring/id/measures/somethingdifferent",
 	}
 
-	timestamp, _ := time.Parse(time.RFC3339, "2016-01-01T10:30:00Z")
-	r := Reading{
-		DateTime: timestamp,
-		Value:    -0.14,
-	}
-
-	metricID := MetricID(s.DataURL)
-	if !validHex.MatchString(metricID) {
-		t.Error("MetricID ot valid hex string", metricID)
-	}
-
-	insertID := InsertID(s, r)
-	if !validHex.MatchString(insertID) {
-		t.Error("InsertID not valid hex string", insertID)
-	}
-	r.DateTime, _ = time.Parse(time.RFC3339, "2016-01-01T11:30:00Z")
-	if insertID == InsertID(s, r) {
-		t.Error("insertID does not change on timestamp", insertID, InsertID(s, r))
+	if s1.UUID() == s2.UUID() {
+		t.Error("UUID clash", s1.UUID(), s2.UUID())
 	}
 }
