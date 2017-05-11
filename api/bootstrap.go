9a2267cad9d538c0b94160e6787277b224549dd6
Rob Tuley <rob@openknot.com>
2017-05-11 23:21:12 +0100

    Fixed use of resp before error checks

--- api/bootstrap.go
+++ api/bootstrap.go
@@ -12,15 +12,18 @@ func bootstrapSnapshots(url string) (*bytes.Buffer, error) {
 	client := &http.Client{
 		Timeout: 15 * time.Second,
 	}
+
 	req, err := http.NewRequest("GET", url, nil)
 	if err != nil {
 		return new(bytes.Buffer), err
 	}
+
 	resp, err := client.Do(req)
-	defer resp.Body.Close()
 	if err != nil {
 		return new(bytes.Buffer), err
 	}
+	defer resp.Body.Close()
+
 	if resp.StatusCode != http.StatusOK {
 		return new(bytes.Buffer), errors.New("Status code " + strconv.Itoa(resp.StatusCode) + " for " + url)
 	}
