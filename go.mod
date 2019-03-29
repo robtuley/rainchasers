4f729f1f547808791d5daa780fd7d495daa6b637
Rob Tuley <rob@openknot.com>
2019-03-29 23:20:21 +0000

    Added river code generator from yaml

--- go.mod
+++ go.mod
@@ -1,15 +1,11 @@
-module github.com/rainchasers/com.rainchasers.gauge
+module github.com/rainchasers/content
 
 require (
 	cloud.google.com/go v0.36.0
 	github.com/algolia/algoliasearch-client-go v2.25.0+incompatible
+	github.com/rainchasers/com.rainchasers.gauge v0.0.0-20190329212636-8d72429b8a73
 	github.com/rainchasers/report v0.0.0-20190320170759-ac9d157ad87e
 	github.com/thingful/osgridconverter v0.0.0-20170127114542-06789ac2e515
-	go.opencensus.io v0.19.0 // indirect
 	go.uber.org/goleak v0.10.0
-	golang.org/x/net v0.0.0-20190213061140-3a22650c66bd // indirect
-	golang.org/x/oauth2 v0.0.0-20190219183015-4b83411ed2b3 // indirect
-	golang.org/x/sys v0.0.0-20190219092855-153ac476189d // indirect
-	google.golang.org/genproto v0.0.0-20190219182410-082222b4a5c5 // indirect
 	google.golang.org/grpc v1.18.0
 )
---
+++ internal/river/section.go
@@ -0,0 +1,53 @@
+package river
+
+import (
+	"crypto/sha1"
+	"encoding/gob"
+	"encoding/hex"
+)
+
+// LatLng is a geographic location
+type LatLng struct {
+	Lat float32 `firestore:"lat" yaml:"lat"`
+	Lng float32 `firestore:"lng" yaml:"lng"`
+}
+
+// Grade is the section difficultly
+type Grade struct {
+	Human   string  `firestore:"text" yaml:"text"`
+	Average float32 `firestore:"value" yaml:"value"`
+	Max     float32 `firestore:"max,omitempty" yaml:"max,omitempty"`
+}
+
+// Calibration is a referenced gauge related to a section
+type Calibration struct {
+	URL         string  `firestore:"-" yaml:"data_url"`
+	Description string  `firestore:"desc" yaml:"desc"`
+	Scrape      float32 `firestore:"scrape,omitempty" yaml:"scrape,omitempty"`
+	Low         float32 `firestore:"low,omitempty" yaml:"low,omitempty"`
+	Medium      float32 `firestore:"medium,omitempty" yaml:"medium,omitempty"`
+	High        float32 `firestore:"high,omitempty" yaml:"high,omitempty"`
+	Huge        float32 `firestore:"huge,omitempty" yaml:"huge,omitempty"`
+	TooHigh     float32 `firestore:"toohigh,omitempty" yaml:"toohigh,omitempty"`
+}
+
+// Section is a paddleable river section
+type Section struct {
+	UUID        string        `firestore:"uuid" yaml:"uuid"`
+	SectionName string        `firestore:"section" yaml:"section"`
+	RiverName   string        `firestore:"river" yaml:"river"`
+	KM          float32       `firestore:"km" yaml:"km"`
+	Grade       Grade         `firestore:"grade" yaml:"grade"`
+	Putin       LatLng        `firestore:"putin" yaml:"putin"`
+	Takeout     LatLng        `firestore:"takeout" yaml:"takeout"`
+	Description string        `firestore:"desc" yaml:"desc"`
+	Directions  string        `firestore:"directions" yaml:"directions"`
+	Measures    []Calibration `firestore:"-" yaml:"measures"`
+}
+
+// Checksum returns a comparable hash of the section
+func (s Section) Checksum() string {
+	b := sha1.New()
+	gob.NewEncoder(b).Encode(s)
+	return hex.EncodeToString(b.Sum(nil))
+}
